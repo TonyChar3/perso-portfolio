27c8c71a7244af8329cc5c8ae742463daa0d682d
Tony Char3 <tonyc6731@gmail.com>
2024-08-19 07:10:35 -0400

    UPDATE: Added a target=_blank to the buttons of Live demo and Github in the project detail page to open a new tab for the user

--- pages/components/project/projectDetailButton.jsx
+++ pages/components/project/projectDetailButton.jsx
@@ -5,6 +5,7 @@ const ProjectDetailButton = ({ title = "", link = "" }) => {
     <>
       <Link
         href={link}
+        target="_blank"
         className="w-40 text-center p-2 md:p-3 rounded-lg bg-[#F7DC6F] active:scale-[0.90] transition-all ease duration-300"
       >
         <p className="text-md md:text-lg font-bold">{title}</p>
